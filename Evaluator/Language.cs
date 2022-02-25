11e325226b9a76e3114c56ce6b68160f2a0b96a3
15906059877 <150270862+15906059877@users.noreply.github.com>
2022-02-25 23:05:53 +0000

    update git aliases

--- Evaluator/Language.cs
+++ Evaluator/Language.cs
@@ -1,4 +1,12 @@
 using System;
 using System.Text;
 
-namespace
+namespace Evaluator
+{
+	/// <summary>
+	/// A base class for basic specifications of syntax for individual languages
+	/// </summary>
+	public abstract class Language
+	{
+		/// <summary>
+		/// Generates a st
