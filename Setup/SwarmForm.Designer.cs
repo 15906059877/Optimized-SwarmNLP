fcadcda372adea0ba2121545c6d3b5669181fa99
15906059877 <150270862+15906059877@users.noreply.github.com>
2023-01-06 12:43:30 +0000

    Bump to 0.0.2

--- Setup/SwarmForm.Designer.cs
+++ Setup/SwarmForm.Designer.cs
@@ -3,4 +3,10 @@ namespace SwarmNLP
     partial class SwarmForm
     {
         /// <summary>
-        /// Required designer
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+    
