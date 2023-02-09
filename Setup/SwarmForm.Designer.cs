5ccad904b4f5a1a0736a624fea42c9d206c80114
15906059877 <150270862+15906059877@users.noreply.github.com>
2023-02-09 08:08:43 +0000

    minor javadoc

--- Setup/SwarmForm.Designer.cs
+++ Setup/SwarmForm.Designer.cs
@@ -99,4 +99,7 @@ namespace SwarmNLP
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontSt
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(13, 103);
+            this.label4.Name = "label4";
+          
