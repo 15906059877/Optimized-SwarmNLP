2502491e564cc5332c8a92785f3f9446a2511896
15906059877 <150270862+15906059877@users.noreply.github.com>
2023-03-01 07:29:07 +0000

    Config changes.

--- Setup/SwarmForm.Designer.cs
+++ Setup/SwarmForm.Designer.cs
@@ -170,4 +170,5 @@ namespace SwarmNLP
             // 
             // label6
             // 
-            this.label6.Auto
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)
