81b57c7ce6866cfe987de70fee20ea2e48e154b2
15906059877 <150270862+15906059877@users.noreply.github.com>
2023-06-17 09:26:03 +0000

    fix sidebar

--- Setup/WindowForm.Designer.cs
+++ Setup/WindowForm.Designer.cs
@@ -31,4 +31,5 @@ namespace SwarmNLP
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WindowForm));
             this.RangeList = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader2 = 
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHe
