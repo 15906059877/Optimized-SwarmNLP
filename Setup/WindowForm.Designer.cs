534cffca4d635affa682e9306566c493fce8484c
15906059877 <150270862+15906059877@users.noreply.github.com>
2023-11-15 08:55:20 +0000

    version history

--- Setup/WindowForm.Designer.cs
+++ Setup/WindowForm.Designer.cs
@@ -119,4 +119,7 @@ namespace SwarmNLP
             // 
             this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label3.Location = new System.Drawing.Point(15, 191);
-   
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(228, 46);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Config.Scaling Res - Decreases rende
